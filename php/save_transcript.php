72ab3c7a4a265939dd222f8f60ce184adcc27acc
noah <noah@noah.jp.net>
2024-09-25 09:48:00 +0100

    forgot openai api key :skull:

--- php/save_transcript.php
+++ php/save_transcript.php
@@ -2,7 +2,7 @@
 // php/save_transcript.php
 
 // Set up OpenAI API credentials
-$openai_api_key = 'YOUR_OPENAI_API_KEY';
+$openai_api_key = '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';
 
 // Handle file upload
 if (isset($_FILES['audio']) && isset($_POST['lectureName']) && isset($_POST['chunkNumber'])) {
